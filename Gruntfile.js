22f3a9962bdf85041276630efc321a862a41f169
Alex Jarvis <alxjrvs@gmail.com>
2014-10-07 11:13:46 -0400

    Added CSS and scss basics

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,15 +15,30 @@ module.exports = function(grunt) {
         }
       }
     },
+    sass: {
+      dist: {
+        files: {
+          'css/app.css': 'css/app.scss'
+        }
+      }
+    },
     watch: {
-      files: 'js/*.coffee',
-      tasks: [ 'coffee' ]
+      files: [
+        'js/*.coffee',
+        'css/*.scss',
+        'css/*.sass'
+      ],
+      tasks: [
+        'coffee',
+        'sass'
+      ]
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['coffee']);
+  grunt.registerTask('default', ['coffee', 'sass']);
 
 };
